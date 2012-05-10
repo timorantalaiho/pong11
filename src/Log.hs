907bb7fd3f4c2dba9c2307adc798b97ed1a71c4c
Jussi Vesala <jussi.vesala@iki.fi>
2012-05-10 22:26:28 +0300

    better logging

--- src/Log.hs
+++ src/Log.hs
@@ -6,6 +6,9 @@ import Coordinate
 logStatistics board = do
   let paddleY = Domain.y $ left $ board
       ballY = Coordinate.y $ pos $ ball $ board
-  writeLogLine $ show $ [paddleY, ballY]
+      ballX = Coordinate.x $ pos $ ball $ board
+      width = paddleWidth $ conf $ board
+      distance = ballX - (fromIntegral width)
+  writeLogLine $ show $ (paddleY, ballY, distance)
 
 writeLogLine line = appendFile "/tmp/pong11.log" $ ('\n' : line)
