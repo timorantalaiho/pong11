744bb206db35800d34ad3682f7cf1c311950a82f
Sami Rosendahl <sami.rosendahl@reaktor.fi>
2012-05-11 17:04:37 +0300

    Remove outdated functionality from game logic

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -33,8 +33,6 @@ chooseDirection currentY targetY
   where difference = targetY - currentY
 
 targetY :: State -> Float
---targetY state = Coordinate.y $ Domain.pos $ Domain.ball board
---  where board = head state  
 targetY state =
     Coordinate.y $ traceBallToOurPaddle p v b
   where b = head state
@@ -61,78 +59,37 @@ advanceBall :: Coordinates -> Velocity -> Coordinates
 advanceBall p v =
     vectorFrom p v
 
-nextHit :: State -> Coordinates
-nextHit [] = dummyNextHit []
-nextHit (b:[]) = dummyNextHit []
-nextHit (board:bs) = projectNextHitFrom (board:bs)
-
-projectNextHitFrom :: State -> Coordinates
-projectNextHitFrom (b:bs)
-  | ballStopped velocity = (Coordinates 0 (Coordinate.y $ extractBallCoordinates b))
-  | otherwise = head $ filter (\c -> fst $ (isHit c velocity b)) ballMovements
-  where ballMovements = scanl(vectorFrom)(extractBallCoordinates b)(repeat (nextStep (b:bs)))
-        velocity = ballVelocity (b:bs)
-
 ballStopped :: Velocity -> Bool
 ballStopped v = ((Coordinate.y v) == 0.0) || ((Coordinate.x v) == 0.0)
 
-nextStep :: State -> Velocity
-nextStep s = ballVelocity s
-
-isHit :: Coordinates -> Velocity -> Board -> (Bool, Velocity)
-isHit c v b = (wouldHit, v')
-  where possibleHits = [myPaddleHit, otherPaddleHit, ceilingHit, floorHit]
-        wouldHit = foldl (||) False (map fst possibleHits)
-        myPaddleHit = hitsPaddle c v b
-        otherPaddleHit = hitsOtherPaddle c v b
-        ceilingHit = hitsCeiling c v b
-        floorHit = hitsFloor c v b
-        v'
-          | wouldHit = snd $ head $ filter fst possibleHits
-          | otherwise = v
-
 hitsPaddle :: Coordinates -> Velocity -> Board -> (Bool, Velocity)
 hitsPaddle hit v board = (isHit, v')
-    where isHit = ((x hit) <= (leftWallX board)) && (insideBoardY hit board)
+    where isHit = (x hit) <= (leftWallX board)
           v'
             | isHit = (Coordinates (-Coordinate.x v) (Coordinate.y v))
             | otherwise = v
 
 hitsOtherPaddle :: Coordinates -> Velocity -> Board -> (Bool, Velocity)
 hitsOtherPaddle hit v board = (isHit, v')
-    where isHit = ((x hit) >= (rightWallX board)) && (insideBoardY hit board)
+    where isHit = (x hit) >= (rightWallX board)
           v'
             | isHit = (Coordinates (-Coordinate.x v) (Coordinate.y v))
             | otherwise = v
 
 hitsCeiling :: Coordinates -> Velocity -> Board -> (Bool, Velocity)
 hitsCeiling hit v board = (isHit, v')
-    where isHit = (Coordinate.y hit) <= 0.0 && (insideBoardX hit board)
+    where isHit = (Coordinate.y hit) <= 0.0
           v'
             | isHit = (Coordinates (Coordinate.x v) (-Coordinate.y v))
             | otherwise = v
 
 hitsFloor :: Coordinates -> Velocity -> Board -> (Bool, Velocity)
 hitsFloor hit v board = (isHit, v')
-    where isHit = (Coordinate.y hit) >= (boardHeight board) && (insideBoardX hit board)
+    where isHit = (Coordinate.y hit) >= (boardHeight board)
           v'
             | isHit = (Coordinates (Coordinate.x v) (-Coordinate.y v))
             | otherwise = v
         
-insideBoardX :: Coordinates -> Board -> Bool
-insideBoardX hit board = True --(Coordinate.x hit) >= (leftWallX board) && (Coordinate.x hit) < (rightWallX board)
-
-insideBoardY :: Coordinates -> Board -> Bool        
-insideBoardY hit board = True --(Coordinate.y hit) >= 0.0 && (Coordinate.y hit) < (boardHeight board) 
-        
-nextHits :: State -> [Coordinates]
-nextHits state = [dummyNextHit state]
-
-dummyNextHit :: State -> Coordinates
-dummyNextHit [] = Coordinates 0 0
-dummyNextHit (b:[]) = Coordinates 0 0
-dummyNextHit (board:bs) = Coordinates 0 (Coordinate.y $ extractBallCoordinates board)
-
 -- Test data
 start_v2 = Coordinates 1 1
 start_p = Coordinates 15.0 15.0
