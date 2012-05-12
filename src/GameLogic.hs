b82f53eb0aa6a13dfd11db999a732f4ba3a42459
Timo Rantalaiho <timo.rantalaiho@reaktor.fi>
2012-05-12 03:19:27 +0300

    Add moar test data

--- src/GameLogic.hs
+++ src/GameLogic.hs
@@ -103,4 +103,6 @@ deflectFromWall v = Coordinates (Coordinate.x v) (-Coordinate.y v)
 -- Test data
 start_v2 = Coordinates 1 1
 start_p = Coordinates 15.0 15.0
-board = Board 123456 (Paddle 19 "foo") (Paddle 14 "bar") (Ball (Coordinates 30 40)) (Conf 640 480  50 10 5 15)
+board = Board 123456 (Paddle 19 "foo") (Paddle 14 "bar") (Ball (Coordinates 100 100)) (Conf 640 480  50 10 5 15)
+board' = Board 123471 (Paddle 19 "foo") (Paddle 14 "bar") (Ball (Coordinates 2 2)) (Conf 640 480  50 10 5 15)
+history = [board',board]
